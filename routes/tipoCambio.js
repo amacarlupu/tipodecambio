7af0d714a910d38010695d10895041cca8021ebf
Anderson Joel <amacarlupup@gmail.com>
2020-12-23 13:51:13 -0500

    Primer commit - añadir archivos

---
+++ routes/tipoCambio.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { getLast5Days, getTipoCambio } = require('../controllers/tipoCambioController');
+
+// Obtener tipo de cambio
+router.get('/dolares/:id', async (req, res)=>{
+
+    const fecha = (req.params.id).toString();
+    
+    const arregloFechas = getLast5Days(fecha);
+    if(!arregloFechas){
+        return res.status(400).json({
+            message: 'Fecha no encontrada'
+        });
+    }
+
+    const tipoCambio = await getTipoCambio(arregloFechas);
+    if(!tipoCambio){
+        return res.status(400).json({
+            message:'Tipo de cambio de encontrado'
+        });
+    }
+
+    res.json(tipoCambio);
+    
+})
+
+module.exports = router;
